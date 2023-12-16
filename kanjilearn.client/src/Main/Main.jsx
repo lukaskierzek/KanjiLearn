64cace76030b494852724285d7f1c478711eaa74
lukaskierzek <lukas.kierzek@gmail.com>
2023-12-16 23:53:46 +0100

    Move code from app.jsx to another file and add new information about web page

--- kanjilearn.client/src/Main/Main.jsx
+++ kanjilearn.client/src/Main/Main.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+﻿import { useEffect, useState } from 'react';
 
 export default function App() {
     const [kanji, setKanji] = useState();
@@ -11,10 +11,11 @@ export default function App() {
 
     async function getKanjiData() {
         try {
-            const kanjiResponse = await fetch("api/kanji");
-            if (!kanjiResponse.ok) {
-                throw new Error(`HTTP error: The status is ${kanjiResponse.status}`);
-            }
+            const kanjiResponse = await fetch("api/kanji"); //https://localhost:5173/api/kanji
+
+            if (!kanjiResponse.ok)
+                throw new Error(`HTTP error ${kanjiResponse.status}`);
+
             const kanjiData = await kanjiResponse.json();
             setKanji(kanjiData);
             setError(null);
@@ -33,16 +34,14 @@ export default function App() {
                 {kanji.map(k =>
                     <>
                         <table>
-                            <tr>Character: {k.character}</tr>
-                            <tr>Strokes: {k.strokes}</tr>
-                            <tr>Translation: {k.translation}</tr>
-                            <tr>Readings kunyomi: {k.readings.kunyomi}</tr>
-                            <tr>Readings onyomi: {k.readings.onyomi}</tr>
-                            <tr>Sentences: {k.sentences.map(s =>
-
+                            <tr>Znak: {k.character}</tr>
+                            <tr>Liczba znaków: {k.strokes}</tr>
+                            <tr>Tłumaczenie: {k.translation}</tr>
+                            <tr>Czytanie kunyomi: {k.readings.kunyomi}</tr>
+                            <tr>Czytanie onyomi: {k.readings.onyomi}</tr>
+                            <tr>Zdania: {k.sentences.map(s =>
                                 <>
-                                    <p>{s.sentenceKanji} - [{s.readingKanjiInSentence}]
-                                        - {s.translationReadingKanjiInSentence}</p>
+                                    <p>{s.sentenceKanji} -「{s.readingKanjiInSentence}」- {s.translationReadingKanjiInSentence}</p>
                                     <p>{s.translation}</p>
                                     <p>{s.sentence}</p>
                                     <hr />
@@ -58,9 +57,8 @@ export default function App() {
     return (
         <>
             <h1 id="tabelLabel">Kanji</h1>
-            <p>This component demonstrates fetching data from the server.</p>
-            {loading && <p>Loading Kanji...</p>}
-            {error && (<p>{`There is a problem witch fetching Kanji. Error: "${error}". Please refresh the page.`}</p>)}
+            {loading && <p>Ładowanie Kanji...</p>} {/* Loading Kanji... */}
+            {error && (<p>{`Problem z ładowaniem Kanji. Błąd: "${error}". Odśwież stronę.`}</p>)} {/*There is a problem witch fetching Kanji. Error: "${error}". Please refresh the page.*/ }
             {kanji && contentKanji()}
         </>
     );
